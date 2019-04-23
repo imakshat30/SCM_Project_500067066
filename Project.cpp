8893f69676f9bd11c2a49f004db89c01ae670619
Akshat Kaushik <kaushikakshat007@gmail.com>
2019-04-23 21:03:19 +0530

    Second Commit

--- Project.cpp
+++ Project.cpp
@@ -24,4 +24,21 @@ class report
 			y=year;
 		}
 
-}rep;
+}rep:
+
+class checkUp
+{
+	long admNo;
+	int height,weight,date,month,year;
+	//char sight[30],ears[30],throat[30],tonsils[30],nails[30];
+	//char hygiene[30],teeth[30],remarks[100];
+	char data[7][50];
+	public:
+		void inputData();
+		void displayData();
+		int getAdmno()
+		{
+			return(admNo);
+		}
+
+}c;
