f2c30f3e930f483f4cc7958bb8674b80e4116da3
Akshat Kaushik <kaushikakshat007@gmail.com>
2019-04-23 21:05:09 +0530

    Third Commit

--- Project.cpp
+++ Project.cpp
@@ -42,3 +42,52 @@ class checkUp
 		}
 
 }c;
+
+class student
+{
+	long admno;
+	//order-> sname,mname,fname,addr,fno,mno,bgp;
+	//char sName[50],mName[50],fName[50],addr[50],fNo[10],mNo[10],bGp[3];
+	char data[7][50];
+	public:
+		void inputData();
+		void displayData(int);
+		int getAdmno()
+		{
+			return(admno);
+		}
+
+}s;
+
+//student database functions
+void studentDB();
+void SD_addRecord();
+void SD_deleteRecord();
+void SD_viewAll();
+void SD_modifyRecord();
+void SD_searchRecord();
+
+//medical checkup functions
+void MedicalCheckup();
+void MC_addRecord();
+void MC_searchRecord(long admn);
+void MC_viewAll();
+
+//report functions
+void Report();
+void R_addRecord();
+void R_searchRecord(long);
+void view_date(int sdate, int smonth, int syear,int edate, int emonth, int eyear);
+void view_month(int mon);
+void curDate(int&,int&,int&);
+
+//ui functions
+void init();
+void mainmenu();
+void box(int,int,int,int);
+void cursor(int,int,char*,COLORS);
+void setText(int,int,char*,COLORS,int);
+char* getText(int,int,char*,COLORS,int);
+long getNum(int,int,char*,COLORS);
+void plus(char* s);
+void clear();
